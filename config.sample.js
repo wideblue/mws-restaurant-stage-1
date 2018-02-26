ed2bb973b162d6d966d07075e79624167700f215
Damjan Dvoršek <big.wide.blue@gmail.com>
2018-02-26 16:01:52 +0100

    fix: added meta tags for UTF-8 encoding and viewport
    Meta tags to specify html page encoding and viewport were added to
    the head of  the index.html and restaurant.html. Additionally I
    created config.js script, because  I didn'twant to incloude my google
    maps API key into git commits.

---
+++ config.sample.js
@@ -0,0 +1,13 @@
+
+(function () {
+    var config = {
+        GOOGLE_MAPS_API_KEY: 'YOUR_GOOGLE_MAPS_API_KEY'
+    }
+
+    var script = document.createElement('script');
+    var x = document.getElementsByTagName('script')[0];
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=' + config.GOOGLE_MAPS_API_KEY + '&libraries=places&callback=initMap'
+    script.async = true;
+    script.defer = true;
+    x.parentNode.appendChild(script);
+}());
