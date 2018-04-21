969bd75f4cb73ece19e795686d1ee4947d08d2cb
Damjan Dvoršek <big.wide.blue@gmail.com>
2018-04-21 22:34:10 +0200

    Stage 2:  dist inclouded

---
+++ dist/config.bundle.js
@@ -0,0 +1,85 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/config.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/config.js":
+/*!***********************!*\
+  !*** ./src/config.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("(function() {\n  let config = {\n    GOOGLE_MAPS_API_KEY: 'AIzaSyDZ4pqIaI7ISUPucp4GgDIsADvD6wGpKcs'\n  };\n\n  let script = document.createElement('script');\n  let x = document.getElementsByTagName('script')[0];\n  script.src = `https://maps.googleapis.com/maps/api/js?key=${config.GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`;\n  script.async = true;\n  script.defer = true;\n  x.parentNode.appendChild(script);\n})();\n\n\n//# sourceURL=webpack:///./src/config.js?");
+
+/***/ })
+
+/******/ });
---
+++ dist/precache-manifest.00990ef641377b1cfb847b9aa11c72e2.js
@@ -0,0 +1,96 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd0ac197c58cf9853dc49b6d1d7581cd",
+    "url": "img/7.jpg"
+  },
+  {
+    "url": "icon_96x96.805e685a37fd37966f5a653e21227edb.svg"
+  },
+  {
+    "revision": "fccce0ba3e46e07b46f4",
+    "url": "restaurant.bundle.js"
+  },
+  {
+    "revision": "e818f858b0be7af1c93afbf150d4cc7e",
+    "url": "index.html"
+  },
+  {
+    "revision": "57412e6443423f8f1a5586d9f76c4088",
+    "url": "restaurant.html"
+  },
+  {
+    "revision": "4c956daa9bf148a30bc1e9fed750438d",
+    "url": "img/PhotoNotAvailable.jpg"
+  },
+  {
+    "revision": "ba4260dee2806745957f4ac41a20fa72",
+    "url": "img/9.jpg"
+  },
+  {
+    "revision": "cc074688becddd2725114187fba9471c",
+    "url": "img/1.jpg"
+  },
+  {
+    "revision": "2bd68efbe70c926de6609946e359faa2",
+    "url": "img/10.jpg"
+  },
+  {
+    "revision": "759b34e9a95647fbea0933207f8fc401",
+    "url": "img/2.jpg"
+  },
+  {
+    "revision": "81ee36a32bcfeea00db09f9e08d56cd8",
+    "url": "img/3.jpg"
+  },
+  {
+    "revision": "0a166f0f4e10c36882f97327b3835aec",
+    "url": "img/5.jpg"
+  },
+  {
+    "revision": "6e0e6fb335ba49a4a732591f79000bb4",
+    "url": "img/8.jpg"
+  },
+  {
+    "revision": "23f21d5c53cbd8b0fb2a37af79d0d37f",
+    "url": "img/4.jpg"
+  },
+  {
+    "revision": "eaf1fec4ee66e121cadc608435fec72f",
+    "url": "img/6.jpg"
+  },
+  {
+    "url": "icon_512x512.805e685a37fd37966f5a653e21227edb.svg"
+  },
+  {
+    "url": "icon_384x384.805e685a37fd37966f5a653e21227edb.svg"
+  },
+  {
+    "url": "icon_256x256.805e685a37fd37966f5a653e21227edb.svg"
+  },
+  {
+    "url": "icon_192x192.805e685a37fd37966f5a653e21227edb.svg"
+  },
+  {
+    "url": "icon_128x128.805e685a37fd37966f5a653e21227edb.svg"
+  },
+  {
+    "revision": "500a3defff288a163f63f80b48025716",
+    "url": "data/restaurants.json"
+  },
+  {
+    "revision": "d40aa2bad09f8d5d856982eb48a786b2",
+    "url": "css/styles.css"
+  },
+  {
+    "revision": "3fbe786ce6c75e9da38a382debf86e3a",
+    "url": "css/responsive.css"
+  },
+  {
+    "revision": "c01793b8a5673eb4745f",
+    "url": "config.bundle.js"
+  },
+  {
+    "revision": "a62f61f8e314dd614653",
+    "url": "app.bundle.js"
+  }
+];
