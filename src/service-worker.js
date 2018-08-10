1745da0d0dd6f3a8093df22cf26701c4af9854a1
Damjan Dvoršek <big.wide.blue@gmail.com>
2018-08-10 15:56:02 +0200

    refactor:  change of Workbox plugin to InjectManifest
    Workbox plugin was changed from  GenerateSW to InjectManifest,
     which will allow more custumisation  for service worker

---
+++ src/service-worker.js
@@ -0,0 +1,19 @@
+/**
+ * service worker file where webpack will import precache-manifest
+ * and Workbox runtime libraries
+ */
+
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {
+  "ignoreUrlParametersMatching": [/id/]
+});
--- webpack.config.js
+++ webpack.config.js
@@ -101,18 +101,8 @@ module.exports = {
         }
       ]
     }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-      ignoreUrlParametersMatching: [/id/]
-      /*  runtimeCaching: [
-        {
-          urlPattern: new RegExp('http://localhost:1337/restaurants'),
-          handler: 'staleWhileRevalidate'
-        }
-      ] */
+    new WorkboxPlugin.InjectManifest({
+      swSrc: './src/service-worker.js'
     })
   ]
 };
